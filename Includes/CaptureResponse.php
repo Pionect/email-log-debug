fbe5930c2ad9ae4cd08680be6771f43fb2b7ead0
Tomas van Rijsse <tomas@pionect.nl>
2016-04-29 00:21:50 +0200

    Seperate the db queries

--- Includes/CaptureResponse.php
+++ Includes/CaptureResponse.php
@@ -7,13 +7,14 @@ class CaptureResponse {
     private $token;
     private $email_id;
     private $table_name;
-    
+
     function __construct(){ 
         // Register hooks
         add_filter( 'wp_mail', array( $this, 'set_token' ),1);
         add_action('phpmailer_init',array(&$this,'phpmailer_init'));
     }
 
+    //set the token early so it is saved by Email Log
     function set_token($mail_info){
         $this->token = uniqid();
         $mail_info['headers'][] = 'DEBUG-TOKEN:'.$this->token;
@@ -30,31 +31,42 @@ class CaptureResponse {
 
         $this->email_id = $wpdb->get_var("SELECT id FROM `{$this->table_name}` WHERE `headers` LIKE '%{$this->token}%'");
 
-
-        $query = $wpdb->prepare(
-            "UPDATE `{$this->table_name}` SET `phpmailer` = %s
-             WHERE `id` = {$this->email_id};",
-            print_r($phpmailer,true));
-        $wpdb->query($query);
+        $this->db_set('phpmailer',print_r($phpmailer,true));
     }
 
     function save_debug_output($string,$debug_level) {
         if($this->token == "") return;
-        
-        global $wpdb;
-        $query = $wpdb->prepare(
-            "UPDATE `{$this->table_name}` SET `smtp_response` = CONCAT(IFNULL(`smtp_response`,''),%s)
-             WHERE `id` = {$this->email_id};",
-            $string);
-        $wpdb->query($query);
+
+        $this->db_append('smtp_response',utf8_encode($string));
     }
     
     function save_result($bool){
         if($this->token == "") return;
 
+        $this->db_set('result',$bool);
+    }
+
+    private function db_set($column,$value){
         global $wpdb;
-        $query = "UPDATE `{$this->table_name}` SET `result` = '$bool'
-                  WHERE `id` = {$this->email_id};";
+        $query = $wpdb->prepare(
+            "UPDATE `{$this->table_name}` SET `$column` = %s
+             WHERE `id` = {$this->email_id};",$value);
+        $wpdb->query($query);
+    }
+
+    private function db_prepend($column,$value){
+        global $wpdb;
+        $query = $wpdb->prepare(
+            "UPDATE `{$this->table_name}` SET `$column` = CONCAT(%s,IFNULL(`$column`,''))
+             WHERE `id` = {$this->email_id};",$value);
+        $wpdb->query($query);
+    }
+
+    private function db_append($column,$value){
+        global $wpdb;
+        $query = $wpdb->prepare(
+            "UPDATE `{$this->table_name}` SET `$column` = CONCAT(IFNULL(`$column`,''),%s)
+             WHERE `id` = {$this->email_id};",$value);
         $wpdb->query($query);
     }
 }
